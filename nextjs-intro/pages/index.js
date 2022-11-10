f1fdd7de451e9ff08594afa40efc13976d096155
hamo-o <yheel369@gmail.com>
2022-11-10 11:21:22 +0900

    2.1 Fetch API

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import MyHead from '../components/MyHead';
 
 export default function home() {
+  const API_KEY = '4a1e120a11deae2f39f59e90bf93826e';
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
+      const { results } = await response.json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <MyHead title="Home" />
-      Home
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <div>{movie.original_title}</div>
+        </div>
+      ))}
     </div>
   );
 }
